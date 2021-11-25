e8ed75bed21195d5edee8f877b77736cae238c82
allinh1 <dl.nguyen1322@gmail.com>
2021-11-25 14:33:40 +1100

    more Card tests

--- src/test/java/org/PokerHandSorter/CardClassTest.java
+++ src/test/java/org/PokerHandSorter/CardClassTest.java
@@ -1,16 +1,25 @@
 package org.PokerHandSorter;
 
+import org.PokerHandSorter.CardTypes.Card;
+import org.PokerHandSorter.Enums.CardSuit;
+import org.PokerHandSorter.Enums.CardValue;
 import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CardClassTest {
 
     @Test
     public void testGetCardValue() {
+        Card testCard = new Card("AH");
+        assertTrue(testCard.getValue() == CardValue.ACE);
 
     }
 
     @Test
     public void testGetCardSuit() {
+        Card testCard = new Card("AH");
+        assertTrue(testCard.getSuit() == CardSuit.HEARTS);
 
     }
 
